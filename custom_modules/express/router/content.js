de20293a4065840459d5f90358f4266f2a96b79b
aapostolou <63057992+aapostolou@users.noreply.github.com>
2021-09-15 13:51:06 +0300

    Create content.js

---
+++ custom_modules/express/router/content.js
@@ -0,0 +1,14 @@
+const content = {
+  menu: [
+    { id: 0, name: "item 0" },
+    { id: 1, name: "item 1" },
+    { id: 2, name: "item 2" },
+    { id: 3, name: "item 3" }
+  ],
+  "contact-info": {
+    phone: "+0123456789",
+    email: "email@test.com"
+  }
+};
+
+module.exports = { content };
